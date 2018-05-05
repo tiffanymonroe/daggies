cc6421e819959457c890dc6b2efe6fc0956f5ac1
Tiffany Monroe <hello@tiffanymonroe.com>
2018-05-04 21:43:50 -0700

    menu responsive

--- app.js
+++ app.js
@@ -7,19 +7,26 @@ app.controller('MainController', ['$http', function($http){
   this.landing = true; //show
   this.menu = true; // show
   this.services = true; //hide
+  this.hamburger = true;
 
+  this.hideHamburger = function(){
+    this.hamburger =! this.hamburger
+  }
 
   this.showLanding = function(){
     this.landing = true;
     this.menu = true;
     this.services = true; //hide
+    this.hamburger = true; //show when closing services menu
   }
 
   this.showServices = function(){
     this.landing = false;
     this.menu = false;
     this.services = false;
+    this.hamburger = false;
   }
+
 
 
 
